83080f2f26854e071149a57d657da0796e70c803
timothe <timothe91200@hotmail.com>
2018-01-16 23:32:47 +0100

    Rajout du select incrémenter des noms des tables

--- ListColonnes.php
+++ ListColonnes.php
@@ -7,11 +7,21 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>liste des nom de colonnes</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+
+        <form method="POST">
+            <?php
+            require_once './controls/SelectTablesName.php';
+            ?>
+            <?php
+            $tNameTables = getTablesFromBD($cnx, $dbNAme);
+            $lscontenu = tableau2Select($tNameTables);
+
+            print $lscontenu;
+            ?>
+            <button type="submit" value="1" name="btValider">Valider</button>
+        </form>
     </body>
 </html>
---
+++ controls/SelectTablesName.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'lib/Metabase.php';
+require_once 'lib/Connexion.php';
+session_start();
+
+//$variable = $_SESSION["variable"];
+$Serveur = $_SESSION["serveur"];
+$Port = $_SESSION["port"];
+$Ut = $_SESSION["ut"];
+$MDP = $_SESSION["mdp"];
+$dbNAme = filter_input(INPUT_GET, "tableName");
+
+$cnx = seConnecter($Serveur, $Port, $Ut, $MDP, $dbNAme);
+
+
+
+function tableau2Select($pTableau) {
+    $lsContenu = "<select name='select'>\n";
+    foreach ($pTableau as $value) {
+        $lsContenu.="<option value=$value>$value</option>\n";
+    }
+    $lsContenu.="</select>";
+
+    return $lsContenu;
+}
